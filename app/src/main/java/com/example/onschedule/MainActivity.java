42e57cce681c106ea235484000ba1b8e38e1cd9d
Fredy Camas <fredycamasdev@gmail.com>
2023-04-11 14:22:00 -0400

    added main private var

--- app/src/main/java/com/example/onschedule/MainActivity.java
+++ app/src/main/java/com/example/onschedule/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.onschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerview;
+    private Button nextActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
