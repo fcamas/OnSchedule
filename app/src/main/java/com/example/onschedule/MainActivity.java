180db5ec1a91f476eda87ac2b2fc516eb001e3f5
Fredy Camas <fredycamasdev@gmail.com>
2023-04-13 14:43:00 -0400

    added new activity card

--- app/src/main/java/com/example/onschedule/MainActivity.java
+++ app/src/main/java/com/example/onschedule/MainActivity.java
@@ -31,6 +31,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
             }
         });
-        
+
     }
 }
---
+++ app/src/main/java/com/example/onschedule/NailsCreateActivity.java
@@ -0,0 +1,14 @@
+package com.example.onschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NailsCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nails_create);
+    }
+}
