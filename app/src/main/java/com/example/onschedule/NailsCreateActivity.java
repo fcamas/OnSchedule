4f84db0d67ef2992f21c28b3b477a11ba5824aed
Fredy Camas <fredycamasdev@gmail.com>
2023-04-14 14:53:00 -0400

    added on create private var

--- app/src/main/java/com/example/onschedule/NailsCreateActivity.java
+++ app/src/main/java/com/example/onschedule/NailsCreateActivity.java
@@ -3,8 +3,16 @@ package com.example.onschedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class NailsCreateActivity extends AppCompatActivity {
+    private Button mSaveButton;
+    private CheckBox mS1;
+    private CheckBox mS2;
+    private CheckBox mS3;
+    private CheckBox mS4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
