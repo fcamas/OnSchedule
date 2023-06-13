71361c4cb1edf3f840e688cf273760848222a613
Fredy Camas <fredycamasdev@gmail.com>
2023-06-12 20:24:00 -0400

    added db helper static var

--- app/src/main/java/com/example/onschedule/DB/NailDBHelper.java
+++ app/src/main/java/com/example/onschedule/DB/NailDBHelper.java
@@ -1,4 +1,10 @@
 package com.example.onschedule.DB;
 
 public class NailDBHelper {
+    private static  final String DATABASE_NAME = "NAIL_DATABASE";
+    private static  final String TABLE_NAME = "NAIL_TABLE";
+    private static  final String COL_1 = "ID";
+    private static  final String COL_2 = "FULLNAME";
+    private static  final String COL_3 = "EMAIL";
+    private static  final String COL_4 = "PHONE";
 }
