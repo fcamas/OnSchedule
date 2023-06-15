0de6a145a4a4b3415c4401960e8109a70f24371a
Fredy Camas <fredycamasdev@gmail.com>
2023-06-14 20:07:00 -0400

    overrided onCreate on db helper

--- app/src/main/java/com/example/onschedule/DB/NailDBHelper.java
+++ app/src/main/java/com/example/onschedule/DB/NailDBHelper.java
@@ -1,5 +1,10 @@
 package com.example.onschedule.DB;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.annotation.Nullable;
+
 public class NailDBHelper {
     private static  final String DATABASE_NAME = "NAIL_DATABASE";
     private static  final String TABLE_NAME = "NAIL_TABLE";
@@ -11,4 +16,12 @@ public class NailDBHelper {
     private static  final String COL_6 = "SERVICE";
     private static  final String COL_7 = "TASK";
     private static  final String COL_8 = "STATUS";
+    public NailDBHelper(@Nullable Context context ) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT , " +
+                "FULLNAME TEXT,EMAIL TEXT,PHONE TEXT,TIME TEXT, SERVICE TEXT, TASK TEXT, STATUS INTEGER)");
+    }
 }
