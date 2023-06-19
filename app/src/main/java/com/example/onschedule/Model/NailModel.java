ef5b67bdcb5570084354c3a9f8854727cdb17516
Fredy Camas <fredycamasdev@gmail.com>
2023-06-18 20:03:00 -0400

    added private var on nail model

--- app/src/main/java/com/example/onschedule/Model/NailModel.java
+++ app/src/main/java/com/example/onschedule/Model/NailModel.java
@@ -1,4 +1,12 @@
 package com.example.onschedule.Model;
 
 public class NailModel {
+    private  int id;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String time;
+    private String service;
+    private String task;
+    private int status;
 }
