d0d483fe4b6f236e77e355c4e2e3075ba72a2652
Fredy Camas <fredycamasdev@gmail.com>
2023-07-11 20:04:00 -0400

    added nail adapter

---
+++ app/src/main/java/com/example/onschedule/Adapter/NailAdapter.java
@@ -0,0 +1,14 @@
+package com.example.onschedule.Adapter;
+
+import com.example.onschedule.DB.NailDBHelper;
+import com.example.onschedule.MainActivity;
+import com.example.onschedule.Model.NailModel;
+
+import java.util.List;
+
+public class NailAdapter {
+    private List<NailModel> mList;
+    private MainActivity mActivity;
+    private NailDBHelper nailHelper;
+
+}
