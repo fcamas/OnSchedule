9473fb2b2211dbbe74d51c894294f4cfdcab309a
Fredy Camas <fredycamasdev@gmail.com>
2023-07-31 20:05:00 -0400

    added listener folder

---
+++ app/src/main/java/com/example/onschedule/Listeners/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package com.example.onschedule.Listeners;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
