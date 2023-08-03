a644d4d8d448a4a7e217bf702372348521d62fd0
Fredy Camas <fredycamasdev@gmail.com>
2023-08-02 20:08:00 -0400

    overrided onMove in recycle helper

--- app/src/main/java/com/example/onschedule/RecyclingViewHelper/RecyclerViewHelper.java
+++ app/src/main/java/com/example/onschedule/RecyclingViewHelper/RecyclerViewHelper.java
@@ -1,6 +1,8 @@
 package com.example.onschedule.RecyclingViewHelper;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.onschedule.Adapter.NailAdapter;
 
@@ -10,5 +12,9 @@ public class RecyclerViewHelper {
     public RecyclerViewHelper(NailAdapter mAdapter) {
         super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
         this.mAdapter = mAdapter;
+    }
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
     }
 }
