a317e43fbcff8f0bfa83067a691cd0203d41fc9e
Fredy Camas <fredycamasdev@gmail.com>
2023-08-03 20:54:00 -0400

    overrided onSwiped in recycle helper

--- app/src/main/java/com/example/onschedule/RecyclingViewHelper/RecyclerViewHelper.java
+++ app/src/main/java/com/example/onschedule/RecyclingViewHelper/RecyclerViewHelper.java
@@ -1,5 +1,8 @@
 package com.example.onschedule.RecyclingViewHelper;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,5 +19,29 @@ public class RecyclerViewHelper {
     @Override
     public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
         return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        final int position = viewHolder.getAdapterPosition();
+        if (direction == ItemTouchHelper.RIGHT || direction == ItemTouchHelper.LEFT){
+            AlertDialog.Builder builder = new AlertDialog.Builder(mAdapter.getContext());
+            builder.setTitle("Confirm");
+            builder.setMessage("Deleting Card");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    mAdapter.deletTask(position);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    mAdapter.notifyItemChanged(position);
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
     }
 }
